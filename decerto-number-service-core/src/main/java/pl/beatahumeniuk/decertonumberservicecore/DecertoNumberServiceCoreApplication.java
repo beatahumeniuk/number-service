489f618eb2250be284b3021373360df986f9d51d
beatahumeniuk <beatahumeniuk@gmail.com>
2020-09-07 21:43:21 +0200

    initial commit

---
+++ decerto-number-service-core/src/main/java/pl/beatahumeniuk/decertonumberservicecore/DecertoNumberServiceCoreApplication.java
@@ -0,0 +1,13 @@
+package pl.beatahumeniuk.decertonumberservicecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecertoNumberServiceCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecertoNumberServiceCoreApplication.class, args);
+    }
+
+}
